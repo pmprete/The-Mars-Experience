7b3b2e90da86768a45b166ce24567fbb591c5a49
Pedrow <pmprete@gmail.com>
2016-04-24 00:09:21 -0300

    First commit World wrapping tide with highmap and texture, first person camera, phobos skydome and sounds

---
+++ Assets/WrapWorld.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class WrapWorld : MonoBehaviour {
+    
+	void OnTriggerExit (Collider player) {
+	    var position = player.transform.position;
+        var boxCollider = (BoxCollider)this.gameObject.GetComponent(typeof(BoxCollider));
+        var boxCollaiderSize = boxCollider.size;
+        var terrainPosition = Terrain.activeTerrain.transform.position;
+        Debug.Log("Player entered the trigger");
+        Debug.Log("Position x :" + position.x);
+        Debug.Log("Position y :" + position.y);
+        Debug.Log("Position z :" + position.z);
+        Debug.Log("terrain position");
+        Debug.Log("Position x :" + terrainPosition.x);
+        Debug.Log("Position y :" + terrainPosition.y);
+        Debug.Log("Position z :" + terrainPosition.z);
+        Debug.Log("box colaider size");
+        Debug.Log("Position x :" + boxCollaiderSize.x);
+        Debug.Log("Position y :" + boxCollaiderSize.y);
+        Debug.Log("Position z :" + boxCollaiderSize.z);
+        if (position.x >= boxCollaiderSize.x)
+        {
+            position.x = terrainPosition.x + 10;
+        }
+        if (position.z >= boxCollaiderSize.z)
+        {
+            position.z = terrainPosition.z + 10;
+        }
+
+        if (position.x <= terrainPosition.x + 5)
+        {
+            position.x = boxCollaiderSize.x -1;
+        }
+        if (position.z <= terrainPosition.z + 5)
+        {
+            position.z = boxCollaiderSize.z - 1;
+        }
+        
+        position.y = Terrain.activeTerrain.SampleHeight(position);
+        player.transform.position = position;
+    }
+}
---
+++ Assets/displayUI.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class displayUI : MonoBehaviour
+{
+
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myText = GameObject.Find("Text").GetComponent<Text>();
+        myText.color = Color.clear;
+        //UnityEngine.Cursor.visible = true;
+        //Screen.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FadeText();
+
+        /*if (Input.GetKeyDown (KeyCode.Escape)) 
+         
+                {
+                        Screen.lockCursor = false;
+                         
+                }
+                */
+
+
+    }
+
+    void OnMouseOver()
+    {
+        displayInfo = true;
+
+    }
+
+
+
+    void OnMouseExit()
+
+    {
+        displayInfo = false;
+
+    }
+
+
+    void FadeText()
+
+    {
+
+
+        if (displayInfo)
+        {
+
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, new Color(1,1,1,0.6f), fadeTime * Time.deltaTime);
+        }
+
+        else
+        {
+
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+
+
+
+
+    }
+
+
+
+}
